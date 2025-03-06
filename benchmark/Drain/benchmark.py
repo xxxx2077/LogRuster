5417625e5cca275ed369d23a72af2029e3a47fdd
xxxx2077 <13712052798@163.com>
2025-03-06 16:44:20 +0800

    整理代码（删除无用代码以及避免上传多余的csv）

--- benchmark/Drain/benchmark.py
+++ benchmark/Drain/benchmark.py
@@ -169,7 +169,7 @@ benchmark_settings = {
     },
 }
 
-bechmark_result = []
+benchmark_result = []
 for dataset, setting in benchmark_settings.items():
     print("\n=== Evaluation on %s ===" % dataset)
     indir = os.path.join(input_dir, os.path.dirname(setting["log_file"]))
@@ -205,11 +205,11 @@ for dataset, setting in benchmark_settings.items():
         groundtruth=os.path.join(indir, log_file + "_structured.csv"),
         parsedresult=os.path.join(output_dir, log_file + "_structured.csv"),
     )
-    bechmark_result.append([dataset, F1_measure, accuracy])
+    benchmark_result.append([dataset, F1_measure, accuracy])
 
 
 print("\n=== Overall evaluation results ===")
-df_result = pd.DataFrame(bechmark_result, columns=["Dataset", "F1_measure", "Accuracy"])
+df_result = pd.DataFrame(benchmark_result, columns=["Dataset", "F1_measure", "Accuracy"])
 df_result.set_index("Dataset", inplace=True)
 print(df_result)
-df_result.to_csv("Drain_bechmark_result.csv", float_format="%.6f")
+df_result.to_csv("Drain_benchmark_result.csv", float_format="%.6f")
